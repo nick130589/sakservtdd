4b392d88cd61c0a6f2da77aaa4d8e37b47752666
Shane Kumpf <sakserv@sakserv.org>
2012-04-06 08:32:53 -0500

    Through 490 - An ideal hash algorithm

--- TDD/src/com/sakserv/sis/studentinfo/Course.java
+++ TDD/src/com/sakserv/sis/studentinfo/Course.java
@@ -19,5 +19,20 @@ public class Course {
 	public String getNumber() {
 		return number;
 	}
+	
+	@Override
+	public boolean equals(Object object) {
+		if (object == null) {
+			return false;
+		}
+		
+		if (this.getClass() != object.getClass()) {
+			return false;
+		}
+		
+		Course that = (Course)object;
+		return this.department.equals(that.department) && 
+			this.number.equals(that.number);
+	}
 
 }
--- TDD/test/com/sakserv/sis/studentinfo/CourseTest.java
+++ TDD/test/com/sakserv/sis/studentinfo/CourseTest.java
@@ -1,6 +1,13 @@
 package com.sakserv.sis.studentinfo;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
 
@@ -11,6 +18,50 @@ public class CourseTest {
 		Course course = new Course("CMSC", "120");
 		assertEquals("CMSC", course.getDepartment());
 		assertEquals("120", course.getNumber());
+	}
+	
+	@Test
+	public void testEquality() {
+		Course courseA = new Course("CMSC", "120");
+		Course courseAPrime = new Course("CMSC", "120");
+		Course courseAPrime2 = new Course("CMSC", "120");
+		assertEquals(courseA, courseAPrime);
+		
+		Course courseB = new Course("ARTH", "330");
+		assertFalse(courseA.equals(courseB));
+		
+		//Reflexivity
+		assertEquals(courseA, courseA);
+		
+		//transitivity
+		assertEquals(courseA, courseAPrime);
+		assertEquals(courseAPrime, courseAPrime2);
+		assertEquals(courseA, courseAPrime2);
+		
+		//Symmetry
+		assertEquals(courseAPrime, courseA);
+		assertEquals(courseA, courseAPrime);
+		
+		//Consistency
+		assertEquals(courseA, courseAPrime);
+		
+		//Compare to null
+		assertFalse(courseA.equals(null));
+		
+		// Containment
+		List<Course> list = new ArrayList<Course>();
+		list.add(courseA);
+		assertTrue(list.contains(courseAPrime));
+		
+		Map<Course, String> map = new HashMap<Course, String>();
+		map.put(courseA, "");
+		assertTrue(map.containsKey(courseAPrime));
+	}
+	
+	@Test
+	public void testApplesAndOranges() {
+		Course courseA = new Course("CMSC", "120");
+		assertFalse(courseA.equals("CMSC-120"));
 	}
 
 }

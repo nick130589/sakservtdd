d8fe492f5853b24b334101740a524916bcd3bf97
Shane Kumpf <sakserv@sakserv.org>
2012-04-16 08:31:23 -0500

    Pg 566- Data Streams

--- TDD/test/com/sakserv/sis/report/RosterReporterTest.java
+++ TDD/test/com/sakserv/sis/report/RosterReporterTest.java
@@ -3,8 +3,14 @@ package com.sakserv.sis.report;
 import static org.junit.Assert.assertEquals;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.io.StringWriter;
 import java.io.Writer;
 
@@ -16,6 +22,7 @@ import com.sakserv.sis.studentinfo.Course;
 import com.sakserv.sis.studentinfo.CourseSession;
 import com.sakserv.sis.studentinfo.Session;
 import com.sakserv.sis.studentinfo.Student;
+import com.sakserv.sis.ui.StudentUI;
 
 public class RosterReporterTest {
 
@@ -42,24 +49,35 @@ public class RosterReporterTest {
 	@Test
 	public void testFiledReport() throws IOException {
 		final String filename = "testFileReport.txt";
-		new RosterReporter(session).writeReport(filename);
+		try {
+			new RosterReporter(session).writeReport(filename);
 		
-		StringBuffer buffer = new StringBuffer();
-		String line;
+			StringBuffer buffer = new StringBuffer();
+			String line;
 		
-		BufferedReader reader = new BufferedReader(new FileReader(filename));
-		while ((line = reader.readLine()) != null) {
-			buffer.append(String.format(line + "%n"));
+			BufferedReader reader = new BufferedReader(
+					new FileReader(filename));
+			while ((line = reader.readLine()) != null) {
+				buffer.append(String.format(line + "%n"));
+			}
+			assertReportContents(buffer.toString());
 		}
-		reader.close();
-		assertReportContents(buffer.toString());
-		
+		finally {
+			delete(filename);
+		}	
 	}
 	
 	public void assertReportContents(String rosterReport) {
-System.out.println(session.getNumberOfStudents());
 		assertEquals(String.format(RosterReporter.ROSTER_REPORT_HEADER +
 				"A%n" + "B%n" + RosterReporter.ROSTER_REPORT_FOOTER +
 				session.getNumberOfStudents() + "%n"), rosterReport);
 	}
+	
+	public void delete(String filename) {
+		File file = new File(filename);
+		if (file.exists()) {
+			file.deleteOnExit();
+		}
+	}
+	
 }
---
+++ TDD/test/com/sakserv/sis/ui/StudentUITest.java
@@ -0,0 +1,82 @@
+package com.sakserv.sis.ui;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.sakserv.sis.studentinfo.Student;
+
+public class StudentUITest {
+	
+	static private final String name = "Leo Xerces Schmoo";
+
+	@Test
+	public void testCreateStudent() throws IOException {
+		StringBuffer expectedOutput = new StringBuffer();
+		StringBuffer input = new StringBuffer();
+		expectedOutput.append(StudentUI.MENU);
+		byte[] buffer = input.toString().getBytes();
+		
+		InputStream inputStream = new ByteArrayInputStream(buffer);
+		OutputStream outputStream = new ByteArrayOutputStream();
+		
+		InputStream consoleIn = System.in;
+		PrintStream consoleOut = System.out;
+		
+		System.setIn(inputStream);
+		System.setOut(new PrintStream(outputStream));
+		
+		try {
+			StudentUI ui = new StudentUI();
+			ui.run();
+			
+			assertEquals(expectedOutput.toString(), outputStream.toString());
+			assertStudents(ui.getAddedStudents());
+		}
+		finally {
+			System.setIn(consoleIn);
+			System.setOut(consoleOut);
+		}
+	}
+	
+	private String line(String input) {
+		return String.format("%s%n", input);
+	}
+	
+	private void assertStudents(List<Student> students) {
+		if (students.size() > 0) {
+			assertEquals(1, students.size());
+			Student student = students.get(0);
+			assertEquals(name, student.getName());
+		}
+	}
+	
+	private void setup(StringBuffer expectedOutput, StringBuffer input) {
+		expectedOutput.append(StudentUI.MENU);
+		input.append(line(StudentUI.ADD_OPTION));
+		expectedOutput.append(StudentUI.NAME_PROMPT);
+		input.append(line(name));
+		expectedOutput.append(line(StudentUI.ADDED_MESSAGE));
+		expectedOutput.append(StudentUI.MENU);
+		input.append(line(StudentUI.QUIT_OPTION));
+	}
+	
+	public static void main(String[] args) throws IOException {
+		new StudentUI().run();
+	}
+	
+	
+
+}
